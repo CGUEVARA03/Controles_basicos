9d5308eca11856686571d73ef27253d88aaba0b9
Caro <USIS037621@ugb.edu.sv>
2022-02-16 19:30:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/controles_basicos/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.controles_basicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText tNombre = (EditText)this.findViewById(R.id.etNombre);
+        final EditText tApellido = (EditText)this.findViewById(R.id.etApellido);
+        Button bDatos = (Button)this.findViewById(R.id.btEnviarDatos);
+
+        bDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                String nom = tNombre.getText().toString();
+                String ape = tApellido.getText().toString();
+
+                Toast.makeText(getApplicationContext(),"Tu nombre es: "+nom+"  "+ape, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+} 
